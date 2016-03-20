e8d61e6b25a7354fbe7b385231400578879bbf81
Jaan Pullerits <clysuva@gmail.com>
2016-03-20 11:04:17 +0200

    Add message acknowledgment

--- src/on.js
+++ src/on.js
@@ -5,9 +5,10 @@ function on(haveChan, routingKey, callback, queueName){
     return haveChan.then(function (channel) {
         return channel.assertQueue(queueName, {durable: false, autoDelete: true}).then(function (queue) {
             channel.bindQueue(queue.queue, config.exchange, routingKey);
-            return channel.consume(queue.queue, (msg) => callback(
-                eventObject(haveChan, JSON.parse(msg.content.toString()))
-            ));
+            return channel.consume(queue.queue, (msg) => {
+                callback(eventObject(haveChan, JSON.parse(msg.content.toString())))
+                channel.ack(msg);
+            });
         });
     });
 }
